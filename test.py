2e05dae41132e539931802e9edc6f2c3d4e83013
Kyle Johnsen <kjjohnsen@gmail.com>
2022-02-01 11:40:55 -0500

    started list access

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 print("Loading the file")
-f = open("test.txt")
+f = open("data/test.txt")
 print(f)
 lines = f.readlines()
+print(lines)
+lines = [x.strip() for x in lines]
 print(lines)
 
 the_sum = 0
 
-for line in lines:
-    the_sum = the_sum + int(line)
-print("The sum is",the_sum)
+for line in lines: 
+    the_sum = the_sum + int(line.strip())
+
+
+print(f"The sum is {the_sum}")
 
 print("The sum is also",sum([int(x) for x in lines])) #prints the sum of the numbers in lines
+
+
