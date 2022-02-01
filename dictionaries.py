0d7d0a8832552a75908e280e9d6c5b4dec778b4a
Kyle Johnsen <kjjohnsen@gmail.com>
2022-02-01 15:06:32 -0500

    work from today on dictionaries and lists

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+a_dict = {"name":"kyle","address":"Boyd GRSC"}
+print(a_dict["name"])
+
+print("name" in a_dict)
+
+#a_list = [1,2,3]
+
+#print(4 in a_list)
+
+print(list(a_dict.keys()))
+print("kyle" in list(a_dict.values()))
--- lists.py
+++ lists.py
@@ -1,4 +1,41 @@
-a_list = [01,13,95]
-print(a_list[0])
+a_list = [0,13,95,34,56,12]
+a_list.reverse()
+print(a_list)
 
+#a_list.reverse()
 
+a_list.sort()
+
+print(sorted(a_list))
+
+print(a_list)
+
+a_list.append(7)
+
+print(a_list)
+
+a_list.extend([53,27])
+
+print(a_list)
+
+a_list.append([342,234])
+print(a_list)
+
+a_list.pop() #removes the last item (and technically returns it)
+
+print(a_list)
+
+a_list.remove(95) #removes the object 95
+
+print(a_list)
+
+del a_list[3] #deletes the 3rd element
+
+print(a_list)
+
+a_list = a_list[0:3] #removes all but the first 3 elements
+
+print(a_list)
+
+a_list.insert(2,3)
+print(a_list)
