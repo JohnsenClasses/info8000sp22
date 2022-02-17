e58f7fd7197f211f9d541aba688ec870620cbb15
Kyle Johnsen <kjjohnsen@gmail.com>
2022-02-17 13:50:18 -0500

    fixed hidden dunder variables

--- CylinderBucket.py
+++ CylinderBucket.py
@@ -3,31 +3,31 @@ class CylinderBucket:
     def __init__(self,height_cm,radius_cm):
         # Input: height of the cylinder, and the radius of the cylinder
         # Output: an initialized cylinder, with properties height, radius
-        self._maxvolume = (math.pi)*(radius_cm**2)*(height_cm)
+        self.__maxvolume = (math.pi)*(radius_cm**2)*(height_cm)
         self.height = height_cm
         self.radius = radius_cm
-        self._currentheight = 0
-        self._area = math.pi*self.radius**2
+        self.__currentheight = 0
+        self.__area = math.pi*self.radius**2
         pass
     def fill(self,quantity_cm3):
         # Input: a volume of water to fill the bucket with
         # Output: the amount of water put in, less any that spilled over
 
         #convert volume to height
-        height = quantity_cm3/self._area
+        height = quantity_cm3/self.__area
 
         #determine how much spilled over by height
-        if self._currentheight + height > self.height:
-            height_over = self._currentheight + height - self.height
-            volume_over = height_over*self._area
-            self._currentheight = self.height
+        if self.__currentheight + height > self.height:
+            height_over = self.__currentheight + height - self.height
+            volume_over = height_over*self.__area
+            self.__currentheight = self.height
             return quantity_cm3 - volume_over
         else:
-            self._currentheight += height
+            self.__currentheight += height
             return quantity_cm3
         pass
     def fill_level(self):
         # Input: None
         # Output: the height of the water so far
-        return self._currentheight
+        return self.__currentheight
         pass
--- test_CylinderBucket.py
+++ test_CylinderBucket.py
@@ -1,6 +1,11 @@
 from CylinderBucket import CylinderBucket
 bucket = CylinderBucket(50,5)
-print(bucket._maxvolume)
 #CylinderBucket.fill(bucket,150)
 print(bucket.fill(4000))
 print(bucket.fill_level())
+#illustrating that you can only access the "hidden" internal __maxvolume through a special name
+print(bucket._CylinderBucket__maxvolume)
+try:
+	print(bucket.__maxvolume) #this line fails
+except:
+	print("Failed, because __maxvolume is hidden")
